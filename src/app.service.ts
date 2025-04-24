3e7072b8482441f826eabd31d0b537d9e52d0ee2
Nguyễn Anh Hào <anhhao012004@gmail.com>
2025-04-24 20:19:30 +0700

    Update greeting message in AppService for consistency

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello ca nha iu cua Kem, dong nay la de test CI/CD!';
+    return 'Hello world!';
   }
 }
